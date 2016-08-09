f5044bb0a5be389c523d793a475907b6b5672447
abhishekmynam <abhishek.mynam@gmail.com>
2016-08-09 15:05:38 -0400

    added test code to write and read from DB
    the newly added code will be deleted after testing

--- searchIntenet.py
+++ searchIntenet.py
@@ -1,6 +1,12 @@
 '''search the internet for the product'''
 import urllib.request
 import json
+from insertUpdateProd import insertUpdateProds
+from trackUserSearch import updateUserSearch
+from prevSearch import getPrevSearch
+
+
+
 '''from amazon.api import AmazonAPI
 AMAZON_ACCESS_KEY="AKIAIWOIGWKW3HFXWP2Q"
 AMAZON_SECRET_KEY="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"
@@ -16,6 +22,9 @@ for i, product in enumerate(products):
         print (organization.get('name'))`
 
 '''
+
+
+
 bestBuyApiKey="dwg5j4bky4vpvcpsvqt7nbuc"
 productName="iphone"
 urlString = "https://api.bestbuy.com/v1/products((search="+productName+"))?apiKey="+bestBuyApiKey+"&sort=salePrice.asc&show=salePrice,url&pageSize=1&callback=JSON_CALLBACK&format=json"
@@ -26,4 +35,15 @@ response_string= response_string.replace("})","}")
 response_json = json.loads(response_string)
 prodPrice = response_json["products"][0]["salePrice"]
 produrl = response_json["products"][0]["url"]
-prods=prodPrice
+product ={"name":productName,"price":prodPrice,"url":produrl}
+
+
+user =123
+writeToDB = insertUpdateProds(product)
+writeToDB.prodManipulations()
+class2 = updateUserSearch(product,user)
+class2.insertUpdateUserSearch()
+classes = getPrevSearch(user)
+classes.prevSearch()
+
+
