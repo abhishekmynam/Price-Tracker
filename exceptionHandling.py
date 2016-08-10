1e6475669efd8d235eddaea9d4beb69e00f6202c
abhishekmynam <abhishek.mynam@gmail.com>
2016-08-10 11:02:35 -0400

    exception handling class
    initial draft

---
+++ exceptionHandling.py
@@ -0,0 +1,16 @@
+'''exceptions are handled in all functions/methods and methods in this file are being called
+    methonds in this file will show a default error message and store the time and error message in the DB table exceptionsLogged'''
+
+import pymongo
+import datetime as dt
+
+DBClient = pymongo.MongoClient()
+DB = DBClient.priceTrackerDB
+
+class exceptionHandler(object):
+    def __init__(self, exceptions, method):
+        self.excep = exceptions
+        self.method=method
+
+    def logInDB (self):
+        DB.exceptionsLogged.insert_one({"loggedDate":dt.datetime.now(),"exception":self.excep,"methodName":self.method})
