d874aef722c14b1ce85e7e4b623bb6412d1938bb
abhishekmynam <abhishek.mynam@gmail.com>
2016-08-10 11:03:05 -0400

    wrapped best buy data fetch in a class and method

--- searchIntenet.py
+++ searchIntenet.py
@@ -3,14 +3,10 @@ import urllib.request
 import json
 from insertUpdateProd import insertUpdateProds
 from trackUserSearch import updateUserSearch
-from prevSearch import getPrevSearch
-
 
 
 '''from amazon.api import AmazonAPI
-AMAZON_ACCESS_KEY="AKIAIWOIGWKW3HFXWP2Q"
-AMAZON_SECRET_KEY="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"
-AMAZON_ASSOC_TAG= "abhiassocaws-20"
+
 amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
 products = amazon.search(Keywords='kindle', SearchIndex='All')
 for i, product in enumerate(products):
@@ -24,28 +20,31 @@ for i, product in enumerate(products):
 '''
 
 
+class internetSearch(object):
+   def __init__(self,productName,userId):
+       self.productName = productName
+       self.userId=userId
 
-bestBuyApiKey="dwg5j4bky4vpvcpsvqt7nbuc"
-user =123
-productName="xbox"
-urlString = "https://api.bestbuy.com/v1/products((search="+productName+"))?apiKey="+bestBuyApiKey+"&sort=salePrice.asc&show=salePrice,url&pageSize=1&callback=JSON_CALLBACK&format=json"
-response= urllib.request.urlopen(urlString)
-response_string=response.read().decode("utf-8")
-response_string =response_string.replace("JSON_CALLBACK({","{")
-response_string= response_string.replace("})","}")
-response_json = json.loads(response_string)
-prodPrice = response_json["products"][0]["salePrice"]
-produrl = response_json["products"][0]["url"]
-product ={"name":productName,"price":prodPrice,"url":produrl}
+   def bestBuyData (self):
+       bestBuyApiKey="dwg5j4bky4vpvcpsvqt7nbuc"
+       user =123
+       self.productName="xbox"
+       urlString = "https://api.bestbuy.com/v1/products((search="+self.productName+"))?apiKey="+bestBuyApiKey+"&sort=salePrice.asc&show=salePrice,url&pageSize=1&callback=JSON_CALLBACK&format=json"
+       response= urllib.request.urlopen(urlString)
+       response_string=response.read().decode("utf-8")
+       response_string =response_string.replace("JSON_CALLBACK({","{")
+       response_string= response_string.replace("})","}")
+       response_json = json.loads(response_string)
+       prodPrice = response_json["products"][0]["salePrice"]
+       produrl = response_json["products"][0]["url"]
+       product ={"name":self.productName,"price":prodPrice,"url":produrl}
+       writeToDB = insertUpdateProds(product)
+       writeToDB.prodManipulations()
+       userSearch = updateUserSearch(product,user)
+       userSearch.insertUpdateUserSearch()
 
+'''classes = getPrevSearch(user)
+dicts=classes.prevSearch()'''
 
-
-writeToDB = insertUpdateProds(product)
-writeToDB.prodManipulations()
-class2 = updateUserSearch(product,user)
-class2.insertUpdateUserSearch()
-classes = getPrevSearch(user)
-dicts=classes.prevSearch()
-print(dicts)
 
 
